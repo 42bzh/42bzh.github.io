d9fc9f4aa8d42ba303b80fa958aa87afff920f2e
ping2A <ping@42.bzh>
2026-04-10 09:41:12 +0200

    up

--- pkg/shell_we_dance_wasm.js
+++ pkg/shell_we_dance_wasm.js
@@ -125,7 +125,7 @@ if (Symbol.dispose) ShellWeDanceApp.prototype[Symbol.dispose] = ShellWeDanceApp.
 function __wbg_get_imports() {
     const import0 = {
         __proto__: null,
-        __wbg___wbindgen_throw_be289d5034ed271b: function(arg0, arg1) {
+        __wbg___wbindgen_throw_81fc77679af83bc6: function(arg0, arg1) {
             throw new Error(getStringFromWasm0(arg0, arg1));
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
